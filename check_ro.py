1d127cdd72c766b5ff77b6255c295eb2cce063b6
dexig <dexig@mail.ru>
2017-01-28 02:31:22 +0300

    refactor

--- check_ro.py
+++ check_ro.py
@@ -2,25 +2,33 @@
 import re
 import os
 
-FILE_MOUNTS = 'mounts.txt'
-XFILE = 'djfwegsk34523TEST'
+def check_ro():
+    FILE_MOUNTS = 'mounts.txt'
+    XFILE = 'djfwegsk34523TEST'
+    has_error = False
 
-with open(FILE_MOUNTS, 'r') as file:
-    DATA = file.read()
+    with open(FILE_MOUNTS, 'r') as file:
+        data = file.read()
 
-PATHS = re.findall(r'[ ]\/[\w\/-]*\s', DATA)
+    paths = re.findall(r'[ ]\/[\w\/-]*\s', data)
 
-for idx, path in enumerate(PATHS):
-    path = path.strip()
-    if path[len(path) - 1] != '/':
-        path = path + '/'
-    PATHS[idx] = path
+    for idx, path in enumerate(paths):
+        path = path.strip()
+        if path[len(path) - 1] != '/':
+            path = path + '/'
+        paths[idx] = path
 
-for path in PATHS:
-    path_to_file = os.path.join(path, XFILE)
-    try:
-        open(path_to_file, 'a').close()
-    except IOError as error:
-        print(error)
-    else:
-        os.remove(path_to_file)
+    for path in paths:
+        path_to_file = os.path.join(path, XFILE)
+        try:
+            open(path_to_file, 'a').close()
+        except IOError as error:
+            has_error = True
+            print(error)
+        else:
+            os.remove(path_to_file)
+    if not has_error:
+        print('ok')
+
+if __name__ == '__main__':
+    check_ro()
